4597ec1c51edb2ae17c4ad86eee61d8bcbed877c
Anabella Villagra <villagra.anabella@gmail.com>
2020-11-15 14:47:49 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,24 @@
+function inicioLink() {
+    let element = document.getElementById("inicioDiv");
+    element.scrollIntoView({behavior: "smooth"});
+}
+
+function sobreLink() {
+    let element = document.getElementById("sobreDiv");
+    element.scrollIntoView({behavior: "smooth"});
+}
+
+function contactoLink() {
+    let element = document.getElementById("contactoDiv");
+    element.scrollIntoView();
+}
+
+
+
+function clickMe () {
+    window.open("https://github.com/anabellavillagra");
+}
+
+function clickMe1 () {
+    window.open("https://www.linkedin.com/in/anabella-villagra-77a3001b9");
+}
